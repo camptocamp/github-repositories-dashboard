67865f1dcf362abd7d4fe4bac9772e567752c455
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-10-23 11:56:05 +0200

    Add forge plugin

---
+++ plugins/forge.js
@@ -0,0 +1,48 @@
+dashboard.forge = function(repo) {
+  var r = repositories[repo.name]['repo'];
+  r.contents('master', 'Modulefile', function(err, contents) {
+    if (err) {
+      updateCell(repo.name, 'forge', 'N/A');
+    } else {
+      updateForge(repo.name, contents);
+    }
+  }, false);
+};
+
+function updateForge(name, contents) {
+  var matches = contents.match(/name\s+(?:["'])(\S+)(?:["\'])/);
+  var module = matches[1];
+  var m = module.split('-');
+  // Now get info (version num and url) from the Forge REST API
+  _request('GET', 'http://www.corsproxy.com/forge.puppetlabs.com/users/'+m[0]+'/modules/'+m[1]+'/releases/find.json', null, function(err, res) {
+    var html = '<a href="'+res.file+'">'+res.version+'</a>';
+    updateCell(name, 'forge', html);
+  });
+};
+
+function _request(method, path, data, cb, raw, sync) {
+  function getURL() {
+    var url = path.indexOf('//') >= 0 ? path : API_URL + path;
+    return url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime();
+  }
+
+  var xhr = new XMLHttpRequest();
+  if (!raw) {xhr.dataType = "json";}
+
+  xhr.open(method, getURL(), !sync);
+  if (!sync) {
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status >= 200 && this.status < 300 || this.status === 304) {
+          cb(null, raw ? this.responseText : this.responseText ? JSON.parse(this.responseText) : true, this);
+        } else {
+          cb({path: path, request: this, error: this.status});
+        }
+      }
+    }
+  };
+  xhr.setRequestHeader('Accept','application/vnd.github.raw+json');
+  xhr.setRequestHeader('Content-Type','application/json;charset=UTF-8');
+  data ? xhr.send(JSON.stringify(data)) : xhr.send();
+  if (sync) return xhr.response;
+};
