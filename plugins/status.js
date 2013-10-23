6b6630514080077ed54e1072f06ed70da4d6acad
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-10-23 23:33:49 +0200

    Send state in status.js

--- plugins/status.js
+++ plugins/status.js
@@ -9,17 +9,21 @@ dashboard.status = function(repo) {
         updateCell(repo.name, 'status', 'ERR');
       } else {
         var diff_msg;
+        var state = 'ok';
         if (diff.status == 'ahead') {
           diff_msg = diff.status + ' ('+diff.ahead_by+' commits)';
+          state = 'warn';
         } else if (diff.status == 'behind') {
           diff_msg = diff.status + ' ('+diff.behind_by+' commits)';
+          state = 'warn';
         } else if (diff.status == 'diverged') {
           diff_msg = diff.status + ' ('+diff.behind_by+' behind and '+diff.ahead_by+' ahead)';
+          state = 'err';
         } else {
           diff_msg = diff.status;
         }
         html = '<a href="'+diff.html_url+'">'+diff_msg+'</a>';
-        updateCell(repo.name, 'status', html);
+        updateCell(repo.name, 'status', html, state);
       }
     });
   } else {
