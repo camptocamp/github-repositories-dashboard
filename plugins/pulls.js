9173ffaf760023a1dfe6c73dc8f10972dd3891fc
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-10-27 14:23:43 +0100

    Warn if PRs > 0

--- plugins/pulls.js
+++ plugins/pulls.js
@@ -1,7 +1,13 @@
 dashboard.pulls = function(repo) {
   var r = repositories[repo.name]['repo'];
   r.listPulls('open', function(err, pulls) {
+    var status;
     html = '<a href="https://github.com/'+account+'/'+repo.name+'/pulls">'+pulls.length+'</a>';
-    updateCell(repo.name, 'pulls', html);
+    if (pulls.length > 0) {
+      status = 'warn';
+    } else {
+      status = 'ok';
+    }
+    updateCell(repo.name, 'pulls', html, status);
   });
 }
