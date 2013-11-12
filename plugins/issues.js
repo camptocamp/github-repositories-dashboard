9240a6cf6a8560d9be51677d2c429f3f95b8ab15
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-11-12 11:23:44 +0100

    Add issues plugin

---
+++ plugins/issues.js
@@ -0,0 +1,27 @@
+dashboard.issues = function(repo) {
+  github.getIssues(account, repo.name).list(null, function(err, issues) {
+    var status;
+    var text;
+    var title;
+    if (err) {
+      title = JSON.parse(err.request.response).message;
+      if (err.error == 410) {
+        text = 'N/A';
+        status = 'ok';
+      } else {
+        text = 'ERR';
+        status = 'warn';
+      }
+    } else {
+      text = issues.length;
+      if (issues.length > 0) {
+        status = 'warn';
+      } else {
+        status = 'ok';
+      }
+    }
+    html = '<a href="https://github.com/'+account+'/'+repo.name+'/issues" title="'+title+'">'+text+'</a>';
+    updateCell(repo.name, 'issues', html, status);
+  });
+}
+
