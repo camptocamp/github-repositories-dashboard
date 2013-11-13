e5b9e5ba470490895b06e231edb8837b9078e1f4
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-11-13 16:06:00 +0100

    No author in last commit is not a warning

--- plugins/last_committer.js
+++ plugins/last_committer.js
@@ -3,15 +3,12 @@ dashboard.last_committer = function(repo) {
   r.getCommits(null, function(err, commits) {
     var commit = commits[0];
 
-    var html;
+    var html = '';
     var status = 'unknown';
 
     if (commit.author) {
       var avatar = '<img src="'+commit.author.avatar_url+'" width="30px" />';
       html = '<a href="'+commit.author.html_url+'">'+avatar+'</a>';
-    } else {
-      html = 'UNKNOWN';
-      status = 'warn';
     }
 
     updateCell(repo.name, 'last_committer', html, status);
