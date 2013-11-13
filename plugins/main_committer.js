a6146c9c45cd3ea346fc5aaad59c58473ab6cb0c
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-11-13 12:26:20 +0100

    Add main_committer plugin

---
+++ plugins/main_committer.js
@@ -0,0 +1,28 @@
+dashboard.main_committer = function(repo) {
+  var r = repositories[repo.name]['repo'];
+  r.getCommits(null, function(err, commits) {
+    var authors = {};
+    var main_committer;
+    for (var i=0; i<commits.length; i++) {
+      var author_obj = commits[i].author;
+      var login = author_obj.login;
+      authors[login] = authors[login] || author_obj;
+      authors[login].count = authors[login].count+1 || 1;
+    }
+
+    for (var author in authors) {
+      if (main_committer) {
+        if (authors[author].count > authors[main_committer.login].count) {
+          main_committer = authors[author];
+        }
+      } else {
+        main_committer = authors[author];
+      }
+    }
+    console.log(main_committer);
+    var avatar = '<img src="'+main_committer.avatar_url+'" width="30px" />';
+    var title = main_committer.count+' recent commits';
+    var html = avatar+' <a href="'+main_committer.html_url+'" title="'+title+'">'+main_committer.login+'</a>';
+    updateCell(repo.name, 'main_committer', html);
+  });
+}
