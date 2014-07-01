19886dd28e1ea2f0a68bd63e7705fbebcf95adfa
Raphaël Pinson <raphink@gmail.com>
2014-07-01 22:03:51 +0200

    Use cached cookie values

--- dashboard.js
+++ dashboard.js
@@ -363,15 +363,20 @@ function createCookie(name,value,days) {
 }
 
 function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) {
-      var value = c.substring(nameEQ.length,c.length);
-      cookies[name] = value;
-      return value;
+  var value = cookies[name];
+  if (value) {
+    return value;
+  } else {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) {
+        value = c.substring(nameEQ.length,c.length);
+        cookies[name] = value;
+        return value;
+      }
     }
   }
   return null;
