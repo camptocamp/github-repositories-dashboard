29b621d4c2e3a2fe0c2b9e870ab77e11a94fdcf3
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-01 14:44:01 +0200

    Smaller lock

--- plugins/priv.js
+++ plugins/priv.js
@@ -1,4 +1,4 @@
 dashboard.priv = function(repo) {
-  var html = repo.private ? '<i class="fa fa-lock fa-2x"></i>' : '';
+  var html = repo.private ? '<i class="fa fa-lock"></i>' : '';
   updateCell(repo.name, 'priv', html);
 }
