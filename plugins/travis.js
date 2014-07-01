47eadc2f06e37856a16e9628bd262bdf040019de
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-01 14:33:13 +0200

    Add private Travis calls with token

--- plugins/travis.js
+++ plugins/travis.js
@@ -1,51 +1,52 @@
 dashboard.travis = function(repo) {
-  var travis_url;
   var status = 'unknown';
   if (repo.private) {
-    updateTravisCell(repo.name, 'https://magnum.travis-ci.com/', '', status);
+    travisAPICall('/auth/github', {"github_token": token}, true, 'POST', null, false, function(err, res) {
+      getTravisStatus(repo, true, res.access_token);
+    })
   } else {
-    travisAPICall('/repos/'+account+'/'+repo.name+'/branches/'+repo.default_branch, false, function(err, res) {
-      var msg;
-      if (err) {
-        msg = 'Error while getting Travis status';
-        status = 'unknown';
-      } else {
-        var date = new Date(res.branch.started_at);
-        var date_str = ' on '+date.toLocaleDateString()+' at '+date.toLocaleTimeString();
-        msg = 'Last build state: '+res.branch.state+' (build #'+res.branch.number+date_str+')';
-        switch (res.branch.state) {
-          case 'passed':
-            status = 'ok';
-            break;
-          case 'failed':
-            status = 'err';
-            break;
-          default:
-            status = 'unknown';
-            break;
-        }
-      }
-      updateTravisCell(repo.name, 'https://travis-ci.org/', msg, status);
-    });
+    getTravisStatus(repo, false, null);
   }
 }
 
-function updateTravisCell(name, travis_url, msg, status) {
-  html = '<a href="'+travis_url+account+'/'+name+'">';
-  html += '<img src="'+travis_url+account+'/'+name+'.png#'+new Date().getTime()+'" title="'+msg+' (state='+status+')" />';
-  html += '</a>';
-  updateCell(name, 'travis', html, status);
+function getTravisStatus(repo, priv, travis_token) {
+  travisAPICall('/repos/'+account+'/'+repo.name+'/branches/'+repo.default_branch, null, priv, 'GET', travis_token, false, function(err, res) {
+    var msg;
+    if (err) {
+      msg = 'Error while getting Travis status';
+      status = 'unknown';
+    } else {
+      var date = new Date(res.branch.started_at);
+      var date_str = ' on '+date.toLocaleDateString()+' at '+date.toLocaleTimeString();
+      msg = 'Last build state: '+res.branch.state+' (build #'+res.branch.number+date_str+')';
+      switch (res.branch.state) {
+        case 'passed':
+          status = 'ok';
+          break;
+        case 'failed':
+          status = 'err';
+          break;
+        default:
+          status = 'unknown';
+          break;
+      }
+    }
+    var api = travisAPIURL(priv);
+    updateTravisCell(repo.name, 'https://'+api+'/', repo.default_branch, travis_token, msg, status);
+  });
 }
 
-function travisAPICall(path, use_corsproxy, cb) {
+function travisAPICall(path, data, priv, verb, travis_token, use_corsproxy, cb) {
   var xhr = new XMLHttpRequest();
+  var api = travisAPIURL(priv);
   var url;
   if (use_corsproxy) {
-    url = 'http://www.corsproxy.com/api.travis-ci.org'+path+'?'+ (new Date()).getTime(); 
+    url = 'http://www.corsproxy.com/'+api+path+'?'+ (new Date()).getTime(); 
   } else {
-    url = 'https://api.travis-ci.org'+path+'?'+ (new Date()).getTime(); 
+    url = 'https://'+api+path+'?'+ (new Date()).getTime(); 
   }
-  xhr.open('GET', url, true);
+  xhr.dataType = "json";
+  xhr.open(verb, url, true);
   xhr.onreadystatechange = function () {
     if (this.readyState == 4) {
       if (this.status >= 200 && this.status < 300 || this.status === 304) {
@@ -57,6 +58,27 @@ function travisAPICall(path, use_corsproxy, cb) {
   }
   xhr.setRequestHeader('Accept','application/vnd.travis-ci.2+json');
   xhr.setRequestHeader('Content-Type','application/json;charset=UTF-8');
-  xhr.send();
+  if (travis_token) {
+    xhr.setRequestHeader('Authorization','token "'+travis_token+'"');
+  }
+  data ? xhr.send(JSON.stringify(data)) : xhr.send();
+}
+
+function travisAPIURL(priv) {
+  if (priv) {
+    return 'api.travis-ci.com';
+  } else {
+    return 'api.travis-ci.org';
+  }
+}
+
+function updateTravisCell(name, travis_url, branch, travis_token, msg, status) {
+  var image_src = travis_url+account+'/'+name+'.png?branch='+branch;
+  image_src += travis_token ? '&token='+travis_token : '';
+  image_src += '#'+new Date().getTime();
+  var html = '<a href="'+travis_url+account+'/'+name+'">';
+  html += '<img src="'+image_src+'" title="'+msg+' (state='+status+')" />';
+  html += '</a>';
+  updateCell(name, 'travis', html, status);
 }
 
