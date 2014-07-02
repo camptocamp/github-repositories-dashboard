a47843dab0adcf3fe83cf09e25992ac97a98d46a
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-02 09:59:42 +0200

    Fix urls in status

--- plugins/status.js
+++ plugins/status.js
@@ -12,31 +12,47 @@ dashboard.status = function(repo) {
         var diff_msg;
         var state = 'ok';
         var customkey;
+        var diff_url;
         if (diff.status == 'ahead') {
           diff_msg = '<span title="'+diff.ahead_by+' commits ahead"><i class="fa fa-angle-double-up"></i> ('+diff.ahead_by+')</span>';
+          diff_url = diff.html_url;
+          html = '<a href="'+diff_url+'">'+diff_msg+'</a>';
           state = 'warn';
           customkey = '3';
         } else if (diff.status == 'behind') {
           diff_msg = '<span title="'+diff.behind_by+' commits behind"><i class="fa fa-angle-double-down"></i> ('+diff.behind_by+')</span>';
+          diff_url = invertDiffURL(diff.html_url);
+          html = '<a href="'+diff_url+'">'+diff_msg+'</a>';
           state = 'warn';
           customkey = '2';
         } else if (diff.status == 'diverged') {
-          diff_msg = '<span title="'+diff.behind_by+' commits behind and '+diff.ahead_by+' commits ahead"><i class="fa fa-code-fork"></i> ('+diff.behind_by+')('+diff.ahead_by+')</span>';
+          ahead_url = diff.html_url;
+          behind_url = invertDiffURL(diff.html_url);
+          html = '<span title="'+diff.behind_by+' commits behind and '+diff.ahead_by+' commits ahead"><i class="fa fa-code-fork"></i> ';
+          html += '<a href="'+behind_url+'">('+diff.behind_by+')</a> ';
+          html += '<a href="'+ahead_url+'">('+diff.ahead_by+')</a>';
+          html += '</span>';
           state = 'err';
           customkey = '1';
         } else if (diff.status == 'identical') {
           diff_msg = '<i class="fa fa-check" title="identical"></i>';
+          html = diff_msg;
           state = 'ok';
           customkey = '0';
         } else {
           diff_msg = diff.status;
+          html = diff_msg;
           customkey = '4';
         }
-        html = '<a href="'+diff.html_url+'">'+diff_msg+'</a>';
         updateCell(repo.name, 'status', html, state, customkey);
       }
     });
   } else {
     updateCell(repo.name, 'status', '');
   }
+}
+
+function invertDiffURL(url) {
+  var base = url.match(/\/([^/]+)\.\.\./);
+  return url.replace(base[0], '/')+'...'+base[1];
 }
