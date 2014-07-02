94f0199a892a32bf1b3a406f57e038a9ab37e3ad
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-02 09:39:49 +0200

    Use a span for status

--- plugins/status.js
+++ plugins/status.js
@@ -13,15 +13,15 @@ dashboard.status = function(repo) {
         var state = 'ok';
         var customkey;
         if (diff.status == 'ahead') {
-          diff_msg = '<i class="fa fa-angle-double-up" title="'+diff.ahead_by+' commits ahead"></i> ('+diff.ahead_by+')';
+          diff_msg = '<span title="'+diff.ahead_by+' commits ahead"><i class="fa fa-angle-double-up"></i> ('+diff.ahead_by+')</span>';
           state = 'warn';
           customkey = '3';
         } else if (diff.status == 'behind') {
-          diff_msg = '<i class="fa fa-angle-double-down" title="'+diff.behind_by+' commits behind"></i> ('+diff.behind_by+')';
+          diff_msg = '<span title="'+diff.behind_by+' commits behind"><i class="fa fa-angle-double-down"></i> ('+diff.behind_by+')</span>';
           state = 'warn';
           customkey = '2';
         } else if (diff.status == 'diverged') {
-          diff_msg = '<i class="fa fa-code-fork" title="'+diff.behind_by+' commits behind and '+diff.ahead_by+' commits ahead"></i> ('+diff.behind_by+')('+diff.ahead_by+')';
+          diff_msg = '<span title="'+diff.behind_by+' commits behind and '+diff.ahead_by+' commits ahead"><i class="fa fa-code-fork"></i> ('+diff.behind_by+')('+diff.ahead_by+')</span>';
           state = 'err';
           customkey = '1';
         } else if (diff.status == 'identical') {
