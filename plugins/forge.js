faa886ca7b6323788d8815ae810da27a07655eed
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-02 10:38:31 +0200

    Forge: check for matching tag

--- plugins/forge.js
+++ plugins/forge.js
@@ -6,43 +6,73 @@ dashboard.forge = function(repo) {
         if (err) {
           updateCell(repo.name, 'forge', 'N/A');
         } else {
-          parseModulefile(repo.name, contents);
+          parseModulefile(repo, contents);
         }
       }, false);
     } else {
-      parseMetadataJSON(repo.name, contents);
+      parseMetadataJSON(repo, contents);
     }
   });
 };
 
-function parseMetadataJSON(name, contents) {
+function parseMetadataJSON(repo, contents) {
   var json = JSON.parse(contents);
   var module = json.name;
-  updateForge(name, module);
+  updateForge(repo, module);
 }
 
-function parseModulefile(name, contents) {
+function parseModulefile(repo, contents) {
   var matches = contents.match(/name\s+(?:["'])([^"']+)(?:["\'])/);
   var module = matches[1];
-  updateForge(name, module);
+  updateForge(repo, module);
 }
 
-function updateForge(name, module) {
+function updateForge(repo, module) {
   var m = module.split('-');
   // forge.puppetlabs.com doesn't allow CORS, use a proxy
   if (m[0] == account) {
     forgeAPICall('/users/'+m[0]+'/modules/'+m[1]+'/releases/find.json', true, function(err, res) {
       if (err) {
-        updateCell(name, 'forge', 'ERR');
+        updateCell(repo.name, 'forge', 'ERR');
       } else {
-        var html = '<a href="http://forge.puppetlabs.com'+res.file+'">'+res.version+'</a>';
-        updateCell(name, 'forge', html);
+        updateForgeCell(repo, res.version, 'http://forge.puppetlabs.com'+res.file;
       }
     });
   } else {
-    updateCell(name, 'forge', 'N/A', 'ok');
+    updateCell(repo.name, 'forge', '', 'ok');
   }
 };
+
+function updateForgeCell(repo, version, url) {
+  // Check if there is a tag for the release
+  var r = repositories[repo.name]['repo'];
+  var html = '<a href="'+url+'">'+version+'</a>';
+  var state;
+  r.listTags(function(err, tags) {
+    if (err) {
+      updateCell(repo.name, 'forge', 'Failed to get tags', 'unknown');
+    } else {
+      var tag_url = versionTagURL(tags, version);
+      if (tag_url) {
+        // Tag found, add tag and link
+        html += ' <a href="'+tag_url+'"><i class="fa fa-tag"></i></a>';
+        state = 'ok';
+      } else {
+        // No tag found, it's a warning
+        state = 'warn';
+      }
+    }
+  });
+  updateCell(repo.name, 'forge', html, state);
+}
+
+function versionTagURL(tags, version) {
+  for (var i=0; i<tags.length; i++) {
+    if (tags[i].name == version) {
+      return tags[i].commit.url;
+    }
+  }
+}
 
 function forgeAPICall(path, use_corsproxy, cb) {
   function getURL() {
