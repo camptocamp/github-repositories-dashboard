c854212d21718eaf60c5f52a4dd1b2f5b972e5d6
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-02 09:36:22 +0200

    Use customkeys for status

--- plugins/status.js
+++ plugins/status.js
@@ -11,20 +11,29 @@ dashboard.status = function(repo) {
       } else {
         var diff_msg;
         var state = 'ok';
+        var customkey;
         if (diff.status == 'ahead') {
-          diff_msg = '<i class="fa fa-angle-double-up"></i> ('+diff.ahead_by+')';
+          diff_msg = '<i class="fa fa-angle-double-up" title="'+diff.ahead_by+' commits ahead"></i> ('+diff.ahead_by+')';
           state = 'warn';
+          customkey = '3';
         } else if (diff.status == 'behind') {
-          diff_msg = '<i class="fa fa-angle-double-down"></i> ('+diff.behind_by+')';
+          diff_msg = '<i class="fa fa-angle-double-down" title="'+diff.behind_by+' commits behind"></i> ('+diff.behind_by+')';
           state = 'warn';
+          customkey = '2';
         } else if (diff.status == 'diverged') {
-          diff_msg = '<i class="fa fa-code-fork"></i> ('+diff.behind_by+')('+diff.ahead_by+')';
+          diff_msg = '<i class="fa fa-code-fork" title="'+diff.behind_by+' commits behind and '+diff.ahead_by+' commits ahead"></i> ('+diff.behind_by+')('+diff.ahead_by+')';
           state = 'err';
+          customkey = '1';
+        } else if (diff.status == 'identical') {
+          diff_msg = '<i class="fa fa-check" title="identical"></i>';
+          state = 'ok';
+          customkey = '0';
         } else {
           diff_msg = diff.status;
+          customkey = '4';
         }
         html = '<a href="'+diff.html_url+'">'+diff_msg+'</a>';
-        updateCell(repo.name, 'status', html, state);
+        updateCell(repo.name, 'status', html, state, customkey);
       }
     });
   } else {
