31ba49e8103bf29bf25b3c71b8b83ba0b9cb74dc
Raphaël Pinson <raphink@gmail.com>
2014-07-12 21:33:38 +0200

    Coveralls: set name to repo.name

--- plugins/coveralls.js
+++ plugins/coveralls.js
@@ -1,7 +1,8 @@
 dashboard.coveralls = function(repo) {
   // TODO: Use API to retrieve actually percentage?
-  var link = 'https://coveralls.io/r/'+account+'/'+repo.name;
-  var image_src = 'https://coveralls.io/repos/'+account+'/'+repo.name+'/badge.png';
+  var name = repo.name;
+  var link = 'https://coveralls.io/r/'+account+'/'+name;
+  var image_src = 'https://coveralls.io/repos/'+account+'/'+name+'/badge.png';
   var html = '<a href="'+link+'"><img src="'+image_src+'" title="Coverage badge" /></a>';
   updateCell(name, 'coveralls', html); 
 }
