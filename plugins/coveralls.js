43c2918ce8e946fcaa68e529273052938c3addbc
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-21 15:56:07 +0200

    Add date to coveralls badge

--- plugins/coveralls.js
+++ plugins/coveralls.js
@@ -2,7 +2,7 @@ dashboard.coveralls = function(repo) {
   // TODO: Use API to retrieve actually percentage?
   var name = repo.name;
   var link = 'https://coveralls.io/r/'+account+'/'+name;
-  var image_src = 'https://coveralls.io/repos/'+account+'/'+name+'/badge.png';
+  var image_src = 'https://coveralls.io/repos/'+account+'/'+name+'/badge.png?'+(new Date()).getTime();
   var html = '<a href="'+link+'"><img src="'+image_src+'" title="Coverage badge" /></a>';
   updateCell(name, 'coveralls', html); 
 }
