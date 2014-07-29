e409dbb141c2c7657ed3314afb3a5c2bbbd7f150
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-29 11:38:43 +0200

    Pass forks_count as customkey

--- plugins/forks.js
+++ plugins/forks.js
@@ -1,4 +1,4 @@
 dashboard.forks = function(repo) {
   var html = '<a href="https://github.com/'+account+'/'+repo.name+'/network">'+repo.forks_count+'</a>';
-  updateCell(repo.name, 'forks', html);
+  updateCell(repo.name, 'forks', html, null, repo.forks_count);
 }
