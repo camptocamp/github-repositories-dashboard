18f95c4f3b802f9ce8421da084cab1232d8bb082
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-29 09:45:59 +0200

    Set user-agent for Forge API

--- plugins/forge.js
+++ plugins/forge.js
@@ -32,7 +32,7 @@ function updateForge(repo, module) {
   var m = module.split('-');
   // forge.puppetlabs.com doesn't allow CORS, use a proxy
   if (m[0] == account) {
-    forgeAPICall('/releases?sort_by=release_date&module='+module, true, function(err, res) {
+    forgeAPICall('/releases?sort_by=release_date&module='+module, false, function(err, res) {
       if (err) {
         var html = '<span title="Module does not exist on the forge but has metadata file"><i class="fa fa-times"></i></span>';
         updateForgeCell(repo.name, html, 'warn', '4');
@@ -142,6 +142,7 @@ function forgeAPICall(path, use_corsproxy, cb) {
       }
     }
   };
+  xhr.setRequestHeader('User-Agent', 'GitHub-Repositories-Dashboard/1.0.0');
   xhr.setRequestHeader('Content-Type','application/json;charset=UTF-8');
   xhr.send();
 };
