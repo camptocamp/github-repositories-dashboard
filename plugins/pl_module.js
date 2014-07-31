4a84f17dda241fad78b625bb21dbf0b72528e099
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-31 12:45:06 +0200

    Check if pl module exists using err

--- plugins/pl_module.js
+++ plugins/pl_module.js
@@ -1,12 +1,13 @@
 dashboard.pl_module = function(repo) {
   var m = repo.name.split('-');
   var r = github.getRepo('puppetlabs', 'puppetlabs-'+m[1]);
-  if (r) {
-    r.show(function(err, info) {
-      var html = '<a href="'+r.info.html_url+'"><i class="fa fa-external-link"></i></a>';
-      updateCell(repo.name, 'pl_module', html);
-    });
-  } else {
-    updateCell(repo.name, 'pl_module', '');
-  }
+  r.show(function(err, info) {
+    var html;
+    if (err) {
+      html = '';
+    } else {
+      html = '<a href="'+r.info.html_url+'"><i class="fa fa-external-link"></i></a>';
+    }
+    updateCell(repo.name, 'pl_module', html);
+  });
 }
