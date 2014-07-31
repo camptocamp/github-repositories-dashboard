47afe9827c77c743f980df8db5f7dd0bb4c333bd
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-31 12:50:41 +0200

    Add customkey to pl_module

--- plugins/pl_module.js
+++ plugins/pl_module.js
@@ -3,11 +3,14 @@ dashboard.pl_module = function(repo) {
   var r = github.getRepo('puppetlabs', 'puppetlabs-'+m[1]);
   r.show(function(err, info) {
     var html;
+    var customkey;
     if (err) {
       html = '';
+      customkey = '0';
     } else {
       html = '<a href="'+info.html_url+'"><i class="fa fa-external-link"></i></a>';
+      customkey = '1';
     }
-    updateCell(repo.name, 'pl_module', html);
+    updateCell(repo.name, 'pl_module', html, null, customkey);
   });
 }
