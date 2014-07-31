df73fb08b047ba1780dc9cec28e8cce20f369774
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-07-31 12:41:54 +0200

    Get pl module info from show

--- plugins/pl_module.js
+++ plugins/pl_module.js
@@ -1,9 +1,12 @@
 dashboard.pl_module = function(repo) {
   var m = repo.name.split('-');
   var r = github.getRepo('puppetlabs', 'puppetlabs-'+m[1]);
-  var html = '';
   if (r) {
-    var html = '<a href="'+r.info.html_url+'"><i class="fa fa-external-link"></i></a>';
+    r.show(function(err, info) {
+      var html = '<a href="'+r.info.html_url+'"><i class="fa fa-external-link"></i></a>';
+      updateCell(repo.name, 'pl_module', html);
+    });
+  } else {
+    updateCell(repo.name, 'pl_module', '');
   }
-  updateCell(repo.name, 'pl_module', html);
 }
