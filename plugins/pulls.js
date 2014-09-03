846b5e1a493b7da1dcb57c236a0a5bfd5c815a04
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-09-03 12:17:01 +0200

    Add title tag to forks and pull requests
    When browsing a long repo list, it's not obvious what these numbers
    mean since the table header is not visible.

--- plugins/pulls.js
+++ plugins/pulls.js
@@ -3,7 +3,8 @@ dashboard.pulls = function(repo) {
   r.listPulls('open', function(err, pulls) {
     var status;
     var customkey = 0;
-    html = '<a href="https://github.com/'+account+'/'+repo.name+'/pulls">'+pulls.length+'</a>';
+    var title = pulls.length+' open pull requests';
+    html = '<a href="https://github.com/'+account+'/'+repo.name+'/pulls" title="'+title+'">'+pulls.length+'</a>';
     if (pulls.length > 0) {
       status = 'warn';
       customkey = pulls.length;
